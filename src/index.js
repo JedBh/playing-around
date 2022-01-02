aec012dea63983bfa338926122e844500dbef32d
JedBh <jbenhod@gmail.com>
2022-01-02 17:13:07 +0200

    state and life cycles

--- src/index.js
+++ src/index.js
@@ -39,50 +39,84 @@ const root = document.querySelector("#root");
 
 // setInterval(tick, 1000);
 
-const comment = {
-  author: {
-    name: "Jed Benhod",
-    url: "https://avatars.githubusercontent.com/u/44101778?v=4",
-  },
-  text: "Hope you find React as helpful as it helped me!",
-  date: "2/1/2022",
-};
+// const comment = {
+//   author: {
+//     name: "Jed Benhod",
+//     url: "https://avatars.githubusercontent.com/u/44101778?v=4",
+//   },
+//   text: "Hope you find React as helpful as it helped me!",
+//   date: "2/1/2022",
+// };
 
-class Comment extends React.Component {
+// class Comment extends React.Component {
+//   render() {
+//     return (
+//       <div className="Comment">
+//         <UserInfo user={this.props.author} />
+//         <div className="Comment-text">{this.props.text}</div>
+//         <div className="Comment-date">{this.props.date}</div>
+//       </div>
+//     );
+//   }
+// }
+
+// class UserInfo extends React.Component {
+//   render() {
+//     return (
+//       <div className="UserInfo">
+//         <Avatar user={this.props.user} />
+//         <div className="UserInfo-name">{this.props.author.name}</div>
+//       </div>
+//     );
+//   }
+// }
+
+// class Avatar extends React.Component {
+//   render() {
+//     return (
+//       <img
+//         className="Avatar"
+//         src={this.props.author.avatarUrl}
+//         alt={this.props.author.name}
+//       />
+//     );
+//   }
+// }
+
+// const element = <Comment author={comment} />;
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
   render() {
     return (
-      <div className="Comment">
-        <UserInfo user={this.props.author} />
-        <div className="Comment-text">{this.props.text}</div>
-        <div className="Comment-date">{this.props.date}</div>
+      <div>
+        <h1>Hello World</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
       </div>
     );
   }
 }
 
-class UserInfo extends React.Component {
-  render() {
-    return (
-      <div className="UserInfo">
-        <Avatar user={this.props.user} />
-        <div className="UserInfo-name">{this.props.author.name}</div>
-      </div>
-    );
-  }
-}
+ReactDOM.render(<Clock />, root);
 
-class Avatar extends React.Component {
-  render() {
-    return (
-      <img
-        className="Avatar"
-        src={this.props.author.avatarUrl}
-        alt={this.props.author.name}
-      />
-    );
-  }
-}
-
-const element = <Comment author={comment} />;
-
-ReactDOM.render(element, root);
+// ReactDOM.render(element, root);
