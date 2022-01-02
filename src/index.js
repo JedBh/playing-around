1e647af52b6400d1487e3c4b46e281be7c9d542a
JedBh <jbenhod@gmail.com>
2022-01-02 16:35:32 +0200

    the comment example: code isnt good but I'm learning

--- src/index.js
+++ src/index.js
@@ -1,40 +1,88 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const clock = document.querySelector("#clock");
+// const clock = document.querySelector("#clock");
 const root = document.querySelector("#root");
 
-const tick = () => {
-  const element = (
-    <div>
-      <h1>{new Date().toLocaleTimeString()}</h1>
-    </div>
-  );
-  ReactDOM.render(element, clock);
-};
+// const tick = () => {
+//   const element = (
+//     <div>
+//       <h1>{new Date().toLocaleTimeString()}</h1>
+//     </div>
+//   );
+//   ReactDOM.render(element, clock);
+// };
 
 // components
 
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
-}
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>;
+//   }
+// }
 
-class MyApp extends React.Component {
+// class MyApp extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Welcome name="Gus" />
+//         <Welcome name="Walter" />
+//         <Welcome name="Jesee" />
+//       </div>
+//     );
+//   }
+// }
+
+// const header = <MyApp />;
+
+// ReactDOM.render(header, root);
+
+// setInterval(tick, 1000);
+
+const comment = {
+  author: {
+    name: "Jed Benhod",
+    url: "https://avatars.githubusercontent.com/u/44101778?v=4",
+  },
+  text: "Hope you find React as helpful as it helped me!",
+  date: "2/1/2022",
+};
+
+class Comment extends React.Component {
   render() {
     return (
-      <div>
-        <Welcome name="Gus" />
-        <Welcome name="Walter" />
-        <Welcome name="Jesee" />
+      <div className="Comment">
+        <UserInfo user={this.props.author} />
+        <div className="Comment-text">{this.props.text}</div>
+        <div className="Comment-date">{this.props.date}</div>
       </div>
     );
   }
 }
 
-const header = <MyApp />;
+class UserInfo extends React.Component {
+  render() {
+    return (
+      <div className="UserInfo">
+        <Avatar user={this.props.user} />
+        <div className="UserInfo-name">{this.props.author.name}</div>
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(header, root);
+class Avatar extends React.Component {
+  render() {
+    return (
+      <img
+        className="Avatar"
+        src={this.props.author.avatarUrl}
+        alt={this.props.author.name}
+      />
+    );
+  }
+}
 
-setInterval(tick, 1000);
+const element = <Comment author={comment} />;
+
+ReactDOM.render(element, root);
