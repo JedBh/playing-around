a02c6e1fe92ed48baf2f99b68925e06f5ac30d5d
JedBh <jbenhod@gmail.com>
2022-01-02 18:15:36 +0200

    I wasted time, I will follow the docs and not jump right ahead

--- src/index.js
+++ src/index.js
@@ -4,141 +4,20 @@ import ReactDOM from "react-dom";
 // const clock = document.querySelector("#clock");
 const root = document.querySelector("#root");
 
-// const tick = () => {
-//   const element = (
-//     <div>
-//       <h1>{new Date().toLocaleTimeString()}</h1>
-//     </div>
-//   );
-//   ReactDOM.render(element, clock);
-// };
+const url =
+  "http://api.weatherapi.com/v1/current.json?key=5f49cc1f080c4c2693b121152220201&q=London&aqi=no";
 
-// components
+const weatherApi = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.current;
+};
 
-// class Welcome extends React.Component {
-//   render() {
-//     return <h1>Hello, {this.props.name}</h1>;
-//   }
-// }
+const response = await weatherApi(url).then((data) => {
+  return data;
+});
 
-// class MyApp extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <Welcome name="Gus" />
-//         <Welcome name="Walter" />
-//         <Welcome name="Jesee" />
-//       </div>
-//     );
-//   }
-// }
+console.log(response);
+const element = <p>sss</p>;
 
-// const header = <MyApp />;
-
-// ReactDOM.render(header, root);
-
-// setInterval(tick, 1000);
-
-// const comment = {
-//   author: {
-//     name: "Jed Benhod",
-//     url: "https://avatars.githubusercontent.com/u/44101778?v=4",
-//   },
-//   text: "Hope you find React as helpful as it helped me!",
-//   date: "2/1/2022",
-// };
-
-// class Comment extends React.Component {
-//   render() {
-//     return (
-//       <div className="Comment">
-//         <UserInfo user={this.props.author} />
-//         <div className="Comment-text">{this.props.text}</div>
-//         <div className="Comment-date">{this.props.date}</div>
-//       </div>
-//     );
-//   }
-// }
-
-// class UserInfo extends React.Component {
-//   render() {
-//     return (
-//       <div className="UserInfo">
-//         <Avatar user={this.props.user} />
-//         <div className="UserInfo-name">{this.props.author.name}</div>
-//       </div>
-//     );
-//   }
-// }
-
-// class Avatar extends React.Component {
-//   render() {
-//     return (
-//       <img
-//         className="Avatar"
-//         src={this.props.author.avatarUrl}
-//         alt={this.props.author.name}
-//       />
-//     );
-//   }
-// }
-
-// const element = <Comment author={comment} />;
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-    };
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(() => {
-      this.tick();
-    }, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date(),
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
-      </div>
-    );
-  }
-}
-
-function App() {
-  return (
-    <div>
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-      <Clock />
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, root);
-// ReactDOM.render(element, root);
+ReactDOM.render(element, root);
