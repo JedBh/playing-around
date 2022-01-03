8330d65f7c5fbba572d6bb61f0d5ea1d7fa716c9
JedBh <jbenhod@gmail.com>
2022-01-03 12:23:38 +0200

    handling events: toggle

--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@ import ReactDOM from "react-dom";
 
 const root = document.querySelector("#root");
 
-ReactDOM.render(<App />, root);
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggleOn: true,
+    };
+    // this binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+      isToggleOn: !prevState.isToggleOn,
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(<Toggle />, root);
