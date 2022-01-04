3d65ba5c242a2253aeadaae7faf8e8ed4679998e
JedBh <jbenhod@gmail.com>
2022-01-04 11:27:54 +0200

    starting: conditional rendering

--- src/index.js
+++ src/index.js
@@ -3,40 +3,5 @@ import ReactDOM from "react-dom";
 
 const root = document.querySelector("#root");
 
-class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggleOn: true,
-      increment: 0,
-    };
-    // this binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
-    this.increment = this.increment.bind(this);
-  }
 
-  increment() {
-    this.setState((state) => ({
-      increment: state.increment + 1,
-    }));
-  }
-
-  handleClick() {
-    this.setState((prevState) => ({
-      isToggleOn: !prevState.isToggleOn,
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>
-          {this.state.isToggleOn ? "ON" : "OFF"}
-        </button>
-        <button onClick={this.increment}>{this.state.increment}</button>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<Toggle />, root);
+ReactDOM.render(, root);
