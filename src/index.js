6fbdd45aa43444383fdb76476993ccb8846a5f83
JedBh <jbenhod@gmail.com>
2022-01-06 16:22:04 +0200

    controlled components

--- src/index.js
+++ src/index.js
@@ -3,4 +3,39 @@ import ReactDOM from "react-dom";
 
 const root = document.querySelector("#root");
 
-ReactDOM.render(</>, root);
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("Form was Submitted! With the name: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label for="name">Name:</label>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          ></input>
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<NameForm />, root);
